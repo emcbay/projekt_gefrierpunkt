148c04689f355ae57e2ec2da0a1a37cc3fbadc87
emcbay <emre.baysal@hotmail.com>
2025-11-02 17:14:42 +0100

    added aggregatcheck :)

--- projekt_gefrierpunkt.py
+++ projekt_gefrierpunkt.py
@@ -6,19 +6,39 @@ gas_water = 100
 gas_ethanol = 78.3
 gas_benzin = 30
 
-# liquid_water = 
-# liquid_ethanol = 
-# liquid_benzin = 
+user_input_water = input("Gib eine Temparatur ein in Celsius für Wasser: ")
+user_input_ethanol = input("Gib eine Temparatur ein in Celsius für Ethanol: ")
+user_input_benzin = input("Gib eine Temparatur ein in Celsius für Benzin: ")
 
-user_input = input("Gib eine Temparatur ein in Celsius: ")
-# if type(int(user_input)) or type(float(user_input)):
-#     print(user_input)
-# else:
-#     print("Keine Zahl")
 
 try:
-    number = float(user_input)
-    if number >= frozen_water and number <= gas_water
-    print()
+    number = float(user_input_water)
+    if number >= frozen_water and number <= gas_water:
+        print("Wasser ist flüssig")
+    elif number >= gas_water:
+        print("Wasser ist gasförmig")
+    else:
+        print("Wasser ist gefroren")
+except:
+    print("not a number")
+
+try:
+    number = float(user_input_ethanol)
+    if number >= frozen_ethanol and number <= gas_ethanol:
+        print("Alkohol ist flüssig")
+    elif number >= gas_ethanol:
+        print("Alkohol ist gasförmig")
+    else:
+        print("Alkohol ist gefroren")
+except:
+    print("not a number")
+try:
+    number = float(user_input_benzin)
+    if number >= frozen_benzin and number <= gas_benzin:
+        print("Benzin ist flüssig")
+    elif number >= gas_benzin:
+        print("Benzin ist gasförmig")
+    else:
+        print("Benzin ist gefroren")
 except:
     print("not a number")
